14f224b9a830f83c11c8105b227e50685f68452c
daniel <daniel_ends@yahoo.com>
2023-04-12 21:48:30 -0600

    -added material-core -neater style

--- src/Exam.js
+++ src/Exam.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import deck from './attempt.json';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import {Button, TextField, Typography} from "@mui/material";
 
 const Exam = () => {
 
@@ -26,6 +28,15 @@ const Exam = () => {
 
         if (answer.toLowerCase() === question.answer.toLowerCase()) {
             setIsCorrect(true);
+            const nextQuestionIndex = questions.indexOf(question) + 1;
+            if (nextQuestionIndex < questions.length) {
+                setQuestion(questions[nextQuestionIndex]);
+                setIsCorrect(false);
+                setAnswer("");
+
+            }
+            randomizeQuestions();
+            setShowAnswer(false); // Hide the answer when moving on to the next question
         } else {
             setIsCorrect(false);
         }
@@ -43,43 +54,38 @@ const Exam = () => {
         setQuestions(shuffledQuestions);
     };
 
-    useEffect(() => {
-        randomizeQuestions();
-        setQuestion(questions[0]);
-
-        // When the component unmounts, stop the stopwatch
-    }, [questions, randomizeQuestions]);
-
-    useEffect(() => {
-        // When the user's answer is correct, move on to the next question and randomize the questions
-        if (isCorrect) {
-            const nextQuestionIndex = questions.indexOf(question) + 1;
-            if (nextQuestionIndex < questions.length) {
-                setQuestion(questions[nextQuestionIndex]);
-                setIsCorrect(false);
-            }
-            randomizeQuestions();
-            setShowAnswer(false); // Hide the answer when moving on to the next question
-
-        }
-    }, [isCorrect, question, questions, randomizeQuestions]);
 
 
     return (
-        <div style={{display: 'flex', alignItems:'center', justifyContent: 'center', verticalAlign:  'middle', textAlign:'center', paddingTop:200}}>
-            <form onSubmit={handleSubmit}>
-                <div dangerouslySetInnerHTML={{__html: question.prompt}}></div>
-                <input type="text" value={answer} onChange={(event) => setAnswer(event.target.value)} />
-                <button type="submit">Submit</button>
-                {isCorrect && <p>Correct!</p>}
-                <button type="button" onClick={() => setShowAnswer(true)}>Show Answer</button>
-
-            </form>
-            {/* Render the answer only if the button has been clicked */}
-            <div>
-                {showAnswer && <p >The correct answer is {question.answer}</p>}
-            </div>
-
+        <div style={{margin: 160, padding: 50}}>
+            <Grid2 container direction={"column"} alignItems={"center"} spacing={5}>
+                <Grid2 item xs={8} xl={5}>
+                    <div dangerouslySetInnerHTML={{__html: question.prompt}}></div>
+                </Grid2>
+                <Grid2 item xs={5} xl={5} spacing={10} style={{padding:150}} container direction={"row"}>
+                    <TextField label={"Answer"} onChange={(event) => setAnswer(event.target.value)}>
+                    </TextField>
+                    <Button type={"submit"} variant={"outlined"} size={"large"}
+                            style={{maxWidth: '80px', maxHeight: '55px', minWidth: '80px', minHeight: '55px'}}
+                            onClick={handleSubmit}>
+                        Submit
+                        {isCorrect && <p>Correct!</p>}
+                    </Button>
+                    <Button type={"submit"} variant={"outlined"} size={"large"}
+                            style={{maxWidth: '80px', maxHeight: '55px', minWidth: '80px', minHeight: '55px'}}
+                            onClick={() => setShowAnswer(true)}>
+                        Show Answer
+                        {isCorrect && <p>Correct!</p>}
+                    </Button>
+                    <Typography gutterBottom variant={"caption"}>
+                        {showAnswer && <p style={{color: 'red', flex: 'content',display: 'flex', justifyContent: 'center', alignItems:'center'
+                        }}>The correct answer is {question.answer}</p>}
+                        {showAnswer && <p style={{color: 'red', flex: 'content',display: 'flex', justifyContent: 'center', alignItems:'center',
+                            fontSize:10
+                        }}>loser</p>}
+                    </Typography>
+                </Grid2>
+            </Grid2>
         </div>);
 };
 
